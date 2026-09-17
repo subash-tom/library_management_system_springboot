be04357cbd42565632ee812383e9bad1cb0687e6
subash <subash.a2025cse@sece.ac.in>
2026-09-17 12:05:33 +0530

    repository added

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,28 +1,44 @@
 package com.example.demo.controller;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PutMapping;
 import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
+
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
     private final BookService service;
 
+
     public BookController(BookService service) {
         this.service = service;
     }
     @GetMapping
-    public Book getBook() {
-        return service.getBookById(1L);
+    public List<Book> getAllBook() {
+        return service.getAllBooks();
     }
     @PostMapping
     public Book addBook(@RequestBody Book book) {
         return service.addBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+        return service.updateBook(id, updatedBook);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        service.deleteBook(id);
+        return "Book deleted successfully";
     }
 
 }
--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -23,6 +23,20 @@ public List<Book> getAllBooks() {
     public Book getBookById(Long id) {
         return repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
     }
+public Book updateBook(Long id, Book updatedBook) {
+        Book existingBook = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        existingBook.setTitle(updatedBook.getTitle());
+        existingBook.setAuthor(updatedBook.getAuthor());
+        existingBook.setPrice(updatedBook.getPrice());
+        return repository.save(existingBook);
+    }
+
+    public void deleteBook(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Book not found");
+        }
+        repository.deleteById(id);
+    }
 }
 
 
